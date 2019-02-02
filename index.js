58825309d599413a5f1bb186e19f8d05834a2581
Shubham Awasthi <yesshubham@gmail.com>
2019-02-02 20:29:07 +0530

    shameless copy from the socket.io website

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+	socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
-app.listen(process.env.PORT || 4000, function(){
-    console.log('Your node js server is running');
+http.listen(process.env.PORT||3000, function(){
+  console.log('listening on *:3000');
 });
