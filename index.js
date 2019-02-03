6fd497476fa3aebfafbd01fae5c5fa65535154fa
Shubham Awasthi <yesshubham@gmail.com>
2019-02-03 22:28:27 +0530

    added some styles

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var users = {};
+
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('user joins', function(msg){
+    io.emit('user joins', msg);
+  });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
+  socket.on('user left', function(msg){
+    io.emit('user left', users[msg.id]);
+    console.log('Total users', users.length);
+    delete users[msg.id];
+  })
 	socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', Object.assign({}, users[msg.id], {message: msg.message}));
+  });
+  socket.on('preferences', function(msg){
+    users[msg.id] = msg;
+    io.emit('update user', msg);
   });
 });
 
